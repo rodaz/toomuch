38916bcb19b033913b736ce3aa87d5c3e4c341c2
rodaz <rodaz@i.ua>
2017-01-06 12:07:49 +0200

    ready to go

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+// Static files
 app.use(express.static(__dirname));
 
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Root route
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// Start server
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
